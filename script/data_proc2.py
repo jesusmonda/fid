974a01fda1e845973c093732f330095bb2319486
Roberto Hermoso <rhermoso98@gmail.com>
2021-01-06 13:59:11 +0100

    feat: predict using NB

--- script/data_proc2.py
+++ script/data_proc2.py
@@ -5,6 +5,22 @@ resultado = {}
 
 f = open("day_wise.csv", "r")
 
+Y = 2000  # dummy leap year to allow input X-02-29 (leap day)
+seasons = [('Winter', (date(Y,  1,  1),  date(Y,  3, 20))),
+           ('Spring', (date(Y,  3, 21),  date(Y,  6, 20))),
+           ('Summer', (date(Y,  6, 21),  date(Y,  9, 22))),
+           ('Autumn', (date(Y,  9, 23),  date(Y, 12, 20))),
+           ('Winter', (date(Y, 12, 21),  date(Y, 12, 31)))]
+
+
+def get_season(now):
+    if isinstance(now, datetime):
+        now = now.date()
+    now = now.replace(year=Y)
+    return next(season for season, (start, end) in seasons
+                if start <= now <= end)
+
+
 # Get the first date for normalize the data
 with open('day_wise.csv') as csv_data:
     reader = csv.reader(csv_data, delimiter=',')
@@ -20,19 +36,26 @@ for line in f.readlines():
     key = data[0]  # group by date
 
     if data[0] == 'Date':
-        data.append('New Cases gt 1000')
-        data.append('New Deaths gt 1000')
+        data.append('New Cases gt 5000')
+        data.append('New Deaths gt 5000')
+        data.append('New Recover gt 5000')
+        data.append('Seasson')
     else:
         day = datetime.strptime(data[0], '%Y-%m-%d')
         data[0] = (day - first_day).days
-        if int(data[5]) > 1000:
+        if int(data[5]) > 5000:
             data.append(1)
         else:
             data.append(0)
-        if int(data[6]) > 1000:
+        if int(data[6]) > 5000:
             data.append(1)
         else:
             data.append(0)
+        if int(data[7]) > 5000:
+            data.append(1)
+        else:
+            data.append(0)
+        data.append(get_season(day))
     i = 0
     res = ''
     while i < len(data):
